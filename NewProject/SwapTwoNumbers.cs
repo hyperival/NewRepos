7c8b6d7faa98f55f2da04731dd17036af3ffba2a
Hyperival <sanjaygautam779@gmail.com>
2022-11-05 00:22:36 +0530

    swap two numbers program

---
+++ NewProject/SwapTwoNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRepo222Batch
+{
+    class SwapNumbers
+    {
+        public static void SwapTwoNumbers(int x, int y)//10,20
+        {
+            int temp = 0;
+            Console.WriteLine("Before swap\nx:{0} y:{1}", x, y);
+            temp = x; //10
+            x = y; //20
+            y = temp;//10
+            Console.WriteLine("After swap\nx:{0} y:{1}", x, y);
+        }
+    }
+}
