38122cc719bda73b8cebbf812198fab75de3fb57
Hyperival <sanjaygautam779@gmail.com>
2022-11-05 00:18:31 +0530

    human program

---
+++ NewProject/human.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRepo222Batch
+{
+    public class Human
+    {
+        public static string company;
+        //state of object
+        //instance variable syntax
+        public string personName;
+        public int phoneNumb;
+
+        //method or behaviour
+        public void Talk()
+        {
+            int numb = 20;
+            Console.WriteLine("He/she can talk" + numb);
+        }
+
+        public void HumanDetails()
+        {
+            Console.WriteLine("Name:{0} Phone:{1}", personName, phoneNumb);
+            // Console.WriteLine("Name:"+personName+" Phone:"+phoneNumb);
+        }
+
+    }
+}
