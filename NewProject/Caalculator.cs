e5ae60b6ade4cff22a9f6fb9d23f4c8917162318
Hyperival <sanjaygautam779@gmail.com>
2022-11-05 00:20:48 +0530

    calculator program

---
+++ NewProject/Caalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRepo222Batch
+{
+    public class Calculator
+    {
+        //access-modifier return-type methodName
+        public static void AddTwoNumbers(int firstNumb, int sendNumb)
+        {
+            int result = firstNumb + sendNumb;
+            Console.WriteLine("Sum of two numbers:" + result);
+        }
+    }
+}
